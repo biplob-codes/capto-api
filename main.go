3b9e40508ef4cba2f6d4fabf72bf96c7157d3423
Md. Biplob Talukdar <biplob.codes@gmail.com>
2026-09-04 21:52:08 +0600

    feat: add basic server with health endpoint

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
-
+import (
+	"errors"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+func healthHandler(w http.ResponseWriter,r *http.Request){
+	w.Write([]byte("Server healthy!"))
+}
 func main(){
-	fmt.Println("Hello World")
+    logger:=slog.New(slog.NewJSONHandler(os.Stdout,nil))
+
+	mux:=http.NewServeMux()
+
+	mux.HandleFunc("GET /health" ,healthHandler)
+
+    server:=&http.Server{
+		Addr:":8080",
+		ReadTimeout: 5*time.Second,
+		WriteTimeout: 10*time.Second,
+		IdleTimeout: 60*time.Second,
+		Handler: mux,
+	}
+
+	logger.Info("server starting","port",server.Addr)
+
+	if err:=server.ListenAndServe();err!=nil && !errors.Is(err,http.ErrServerClosed){
+		logger.Error("server failed","error",err)
+		os.Exit(1)
+	}
+    logger.Warn("server stopped")
  }
