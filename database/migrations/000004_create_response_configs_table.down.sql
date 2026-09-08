46be1ff51639ced50d09c4adbfc7be637c785b92
Md. Biplob Talukdar <biplob.codes@gmail.com>
2026-09-09 00:52:47 +0600

    feat(db): create migration for response_configs table

---
+++ database/migrations/000004_create_response_configs_table.down.sql
@@ -0,0 +1,3 @@
+DROP TRIGGER IF EXISTS trg_response_configs_update ON response_configs;
+DROP TABLE IF EXISTS response_configs;
+DROP TYPE IF EXISTS res_method;
---
+++ database/migrations/000004_create_response_configs_table.up.sql
@@ -0,0 +1,19 @@
+CREATE TYPE res_method AS ENUM ('ALL','GET','POST','PUT','PATCH','DELETE','OPTIONS');
+
+CREATE TABLE response_configs(
+id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+method res_method NOT NULL DEFAULT 'ALL',
+endpoint_id UUID NOT NULL,
+delay INT DEFAULT 0,
+headers TEXT DEFAULT '{"Content-Type": "application/json"}',
+body TEXT DEFAULT '{"received": true}',
+status_code INT DEFAULT 200 CHECK (status_code BETWEEN 100 AND 599),
+created_at TIMESTAMPTZ DEFAULT now(),
+updated_at TIMESTAMPTZ DEFAULT now(),
+UNIQUE (endpoint_id,method),
+FOREIGN KEY (endpoint_id) REFERENCES endpoints ON DELETE CASCADE
+);
+
+CREATE TRIGGER trg_response_configs_update 
+BEFORE UPDATE ON response_configs
+FOR EACH ROW EXECUTE FUNCTION set_updated_at();
